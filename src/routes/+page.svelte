835dc8b1d3c12a8d3353c489a94efdcb5030e401
azzam <azzam.devv@gmail.com>
2023-09-08 01:55:38 +0400

    Full first version of the website

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,31 +1,50 @@
 <script>
 	import Linkedin from '$lib/icons/Linkedin.svelte';
 	import Tiktok from '$lib/icons/Tiktok.svelte';
+
+	let bullets = [
+		'Website Design.',
+		'Mobile App Design.',
+		'Graphic Design.'
+	];
 </script>
 
-<div class="grid justify-center items-center">
+<div class="grid justify-center items-center pt-10">
 	<div
-		class="flex flex-col-reverse lg:flex-row border-2 border-[#281f15] p-10 justify-between lg:gap-32 gap-10 mx-8 mb-8"
+		class="flex flex-col-reverse xl:flex-row border-2 border-[#281f15] p-10 justify-between xl:gap-32 gap-10 mx-8 mb-8 rounded-2xl"
 	>
-		<div class="flex flex-col justify-between lg:gap-0 gap-8">
-			<div class="flex flex-col gap-4 max-w-2xl">
-				<h1 class="lg:text-8xl text-3xl font-semibold font-poppins text-[#281f15]">Azzam Safar</h1>
-				<h2 class="lg:text-2xl text-xl font-light font-poppins text-[#281f15]">
+		<div class="flex flex-col justify-between xl:gap-0 gap-8">
+			<div class="flex flex-col gap-8 max-w-2xl">
+				<h1 class="xl:text-8xl text-3xl font-semibold font-poppins text-[#281f15]">Azzam Safar</h1>
+				<h2 class="xl:text-2xl text-xl font-normal font-poppins text-[#281f15]">
 					Hi there! I'm a designer with over 4 years of hands-on experience, working with projects
-					from all over the world and I love creating unforgettable user experiences, I'm skilled in
-					making websites, web apps, and mobile apps. Let's connect and work on something awesome
+					from all over the world and I love creating unforgettable user experiences, Let's connect and work on something awesome
 					together!
 				</h2>
+				<div class="flex flex-col gap-4">
+					<h3 class="xl:text-2xl text-xl font-semibold font-poppins text-[#281f15]">
+						What I Do
+					</h3>
+					<ul class="list-disc font-poppins text-[#281f15] xl:text-lg text-md flex flex-col gap-2">
+						{#each bullets as bullet}
+						<li>{bullet}</li>
+						{/each}
+					</ul>
+				</div>
 			</div>
 			<div class="flex gap-4">
-				<a href="https://www.tiktok.com/@azzam.safar">
+				<a target="_blank" rel="noopener noreferrer" href="https://www.tiktok.com/@azzam.safar">
 					<Tiktok />
 				</a>
-				<a href="https://www.linkedin.com/in/azzam-safar/">
+				<a
+					target="_blank"
+					rel="noopener noreferrer"
+					href="https://www.linkedin.com/in/azzam-safar/"
+				>
 					<Linkedin />
 				</a>
 			</div>
 		</div>
-		<img src="/images/azzam.webp" alt="Azzam Safar's face" class="mx-auto max-h-[40rem]" />
+		<img src="/images/azzam.webp" alt="Azzam Safar's face" class="mx-auto max-h-[40rem] rounded-2xl" />
 	</div>
 </div>
--- src/routes/protfolio/+page.svelte
+++ src/routes/protfolio/+page.svelte
@@ -1,50 +1,124 @@
 <script>
-	import Linkedin from '$lib/icons/Linkedin.svelte';
-	import Tiktok from '$lib/icons/Tiktok.svelte';
-	import { onMount } from 'svelte';
+	let projects = [
+		{
+			title: 'Unideals',
+			description: `Unideals is a student discount app that helps students save money on their favorite brands, I was responsible for creating the full user experience and user interface for the app, also created the full brand guidelines and brand identity, and much more!`,
+			type: 'Mobile App',
+			location: 'London, United Kingdom',
+			languages: 'English, Arabic',
+			bulletsTitle: 'What I Did',
+			bullets: [
+				'Created full user experience & user interface.',
+				'Created full brand guidelines and brand identity.',
+				'Created logo and graphic assets.'
+			],
+			link1: 'https://www.myunideals.com/',
+			link2: 'https://www.linkedin.com/in/azzam-safar/details/recommendations/',
+			image: '/images/app.webp'
+		},
+		{
+			title: 'Macandsoft',
+			description: `Macandsoft is an IT company that provides IT solutions for businesses, I was responsible for creating the full user experience and user interface for the website, also created the full brand guidelines and brand identity, and much more!`,
+			type: 'Website',
+			location: 'Berlin, Germany',
+			languages: 'English, Spanish, German',
+			bulletsTitle: 'What I Did',
+			bullets: [
+				'Created full user experience & user interface.',
+				'Created full brand guidelines and brand identity.',
+				'Created logo and graphic assets.'
+			],
+			link1: 'https://www.macandsoft.com/',
+			link2: 'https://www.linkedin.com/in/azzam-safar/details/recommendations/',
+			image: '/images/macandsoft.webp'
+		},
+		{
+			title: 'Inosio',
+			description: `Inosio is a property management software that helps property managers manage properties. I was responsible for creating the complete user experience and user interface for the web app. Additionally, I developed the entire brand guidelines and brand identity, among other responsibilities.`,
+			type: 'SaaS',
+			location: 'San Diego, United States',
+			languages: 'English, Arabic, Spanish',
+			bulletsTitle: 'What I Did',
+			bullets: [
+				'Created full user experience & user interface.',
+				'Created full brand guidelines and brand identity.',
+				'Created presentations, brochures, logo, and various graphic assets.'
+			],
+			link1: 'https://www.inosio.com/',
+			link2: 'https://www.linkedin.com/in/azzam-safar/details/recommendations/',
+			image: '/images/inosio.webp'
+		}
+	];
+</script>
 
-  let sections = [
-    {
-      name: 'Azzam Safar',
-      description: 'Hi there! I\'m a designer with over 4 years of hands-on experience, working with projects from all over the world and I love creating unforgettable user experiences, I\'m skilled in making websites, web apps, and mobile apps. Let\'s connect and work on something awesome together!',
-      imageSrc: '/images/azzam.webp',
-      socialLinks: [
-        { href: 'https://www.tiktok.com/@azzam.safar', icon: 'Tiktok' },
-        { href: 'https://www.linkedin.com/in/azzam-safar/', icon: 'Linkedin' }
-      ]
-    },
-    // Add more sections here...
-  ];
+{#each projects as project, i}
+	<section class="grid justify-center items-center pt-10">
+		<div
+			class={`flex flex-col-reverse border-2 border-[#281f15] rounded-2xl p-10 justify-between xl:gap-32 gap-10 mx-8 mb-8 ${
+				i % 2 == 0 ? 'xl:flex-row' : 'xl:flex-row-reverse'
+			}`}
+		>
+			<div class="flex flex-col justify-between xl:gap-0 gap-8">
+				<div class="flex flex-col gap-8 max-w-2xl">
+					<div class="flex flex-col gap-3">
+						<h1 class="xl:text-8xl text-3xl font-semibold font-poppins text-[#281f15]">
+							{project.title}
+						</h1>
+						<div class="flex gap-4 flex-wrap text-center">
+							<h3
+								class="bg-[#281f15] rounded-2xl :text-2xl text-md font-medium font-poppins text-white p-2 px-4"
+							>
+								{project.type}
+							</h3>
 
-  onMount(() => {
-    // Any additional initialization logic
-  });
-  export let section: {
-    name: string;
-    description: string;
-    imageSrc: string;
-    socialLinks: { href: string; icon: string }[];
-  };
-  </script>
-
-<div class="grid justify-center items-center">
-	{#each sections as section}
-	  <Section {section} />
-	{/each}
-  </div>
-  
-  <div class="flex flex-col-reverse lg:flex-row border-2 border-[#281f15] p-10 justify-between lg:gap-20 gap-10 mx-8 mb-8">
-	<div class="flex flex-col justify-between lg:gap-0 gap-8">
-	  <div class="flex flex-col gap-4 max-w-2xl">
-		<h1 class="lg:text-8xl text-3xl font-semibold font-poppins text-[#281f15]">{section.name}</h1>
-		<h2 class="lg:text-2xl text-xl font-light font-poppins text-[#281f15]">{section.description}</h2>
-	  </div>
-	  <div class="flex gap-4">
-		{#each section.socialLinks as link}
-		  <a href={link.href}>
-		  </a>
-		{/each}
-	  </div>
-	</div>
-	<img src={section.imageSrc} alt="{section.name}'s face" class="mx-auto max-h-[40rem]" />
-  </div>
+							<h3
+								class="bg-[#281f15] rounded-2xl :text-2xl text-md font-medium font-poppins text-white p-2 px-4"
+							>
+								{project.location}
+							</h3>
+							<h3
+								class="bg-[#281f15] rounded-2xl :text-2xl text-md font-medium font-poppins text-white p-2 px-4"
+							>
+								{project.languages}
+							</h3>
+						</div>
+					</div>
+					<h2 class="xl:text-2xl text-xl font-normal font-poppins text-[#281f15]">
+						{project.description}
+					</h2>
+					<div class="flex flex-col gap-4">
+						<h3 class="xl:text-2xl text-xl font-semibold font-poppins text-[#281f15]">
+							{project.bulletsTitle}
+						</h3>
+						<ul
+							class="list-disc font-poppins text-[#281f15] xl:text-lg text-md flex flex-col gap-2"
+						>
+							{#each project.bullets as bullets}
+								<li>{bullets}</li>
+							{/each}
+						</ul>
+					</div>
+				</div>
+				<div class="flex gap-4 md:flex-row flex-col text-center">
+					<a
+						class="bg-transparent hover:bg-[#281f15] text-[#281f15] hover:text-white border-2 border-[#281f15] rounded-2xl hover:border-transparent font-poppins font-semibold text-xl p-4 w-full"
+						href={project.link1}
+						target="_blank"
+						rel="noopener noreferrer"
+					>
+						Live Project
+					</a>
+					<a
+						class="bg-transparent hover:bg-[#281f15] text-[#281f15] hover:text-white border-2 border-[#281f15] rounded-2xl hover:border-transparent font-poppins font-semibold text-xl p-4 w-full"
+						href={project.link2}
+						target="_blank"
+						rel="noopener noreferrer"
+					>
+						Customer Review
+					</a>
+				</div>
+			</div>
+			<img src={project.image} alt={project.title} class="mx-auto max-h-[40rem]" />
+		</div>
+	</section>
+{/each}

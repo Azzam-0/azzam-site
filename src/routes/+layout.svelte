9a86f3ff7169e5a34c8efab886c872724961198d
azzam <azzam.devv@gmail.com>
2023-09-08 04:21:22 +0400

    updated svelte head from metas in HTML

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,5 +3,12 @@
 	import Navbar from '$lib/components/Navbar.svelte';
 </script>
 
+<svelte:head>
+	<title>Azzam Safar</title>
+	<meta name="description" content="Azzam Safar a designer skilled in website, mobile app, and web app designs, let's chat!" />
+	<meta name="keywords" content="Azzam Safar, Azzam, Safar, Designer, UI, UX, Web, Mobile, App, Website, Web App, Mobile App, Design" />
+	<meta name="author" content="Azzam Safar" />
+</svelte:head>
+
 <Navbar />
 <slot />
